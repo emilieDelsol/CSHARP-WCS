2572261a867c6696c423a987ec47642c4ebc1ecc
emilieDelsol <emiliedelsol1@gmail.com>
2021-01-17 15:00:59 +0100

    add quest asynch await

---
+++ QUESTS/asyncAwaitQuestCsharp/asyncAwaitQuestCsharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace asyncAwaitQuestCsharp
+{
+	class Program
+	{
+            static async Task<Int32> Main(string[] args)
+
+            {
+                var task1 = SomeOpAsync();
+                var task2 = SomeOpAsync();
+
+                await Task.WhenAll(task1, task2);
+
+                Console.WriteLine("Main Thread Completed");
+
+                Console.ReadLine();
+                 return 0;
+
+            }
+
+		private async static Task SomeOpAsync()
+		{
+            Random rng = new Random();
+
+            Int32 waitTime = rng.Next(1000, 7000);
+
+            await Task.Delay(waitTime);
+
+            Console.WriteLine("The task has waited " + waitTime);
+
+		}
+	}
+}
