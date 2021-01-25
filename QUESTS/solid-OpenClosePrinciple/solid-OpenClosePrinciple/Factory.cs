244bb6ba779bf9f8de02099b92512317efec7b6c
emilieDelsol <emiliedelsol1@gmail.com>
2021-01-25 13:09:58 +0100

    add quest solid open close

---
+++ QUESTS/solid-OpenClosePrinciple/solid-OpenClosePrinciple/Factory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace solid_OpenClosePrinciple
+{
+	internal sealed class Factory
+	{
+		internal Production Craft(IEnumerable<Craftable> craftables)
+		{
+			Production production = new Production(craftables);
+			return production;
+		}
+	}
+}
---
+++ QUESTS/solid-OpenClosePrinciple/solid-OpenClosePrinciple/Production.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace solid_OpenClosePrinciple
+{
+	internal class Production
+	{
+        public IEnumerable<Craftable> Materials { get; set; }
+        public Production(IEnumerable<Craftable> manyMaterials)
+
+        {
+            Materials = manyMaterials;
+        }
+        public override string ToString()
+        {
+            string description = "Object is composed of\n:";
+            foreach (Craftable material in Materials)
+            {
+                description += "\t" + material.Type;
+            }
+            return description;
+        }
+    }
+}
